c0e43b9a3f568155ff76d10a38ff8a112107882a
Scottys Guest Laptop <armand.scotty@yahoo.com>
2020-10-26 20:58:09 -0400

    inital implementation of networking features

--- src/BizHawk.Emulation.Common/Networking/NetworkClient.cs
+++ src/BizHawk.Emulation.Common/Networking/NetworkClient.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
 namespace BizHawk.Emulation.Common
 {
@@ -7,13 +13,13 @@ namespace BizHawk.Emulation.Common
 	/// based netcode. But later it should support rollback. Since this requires precise timings we'll use TCP instead
 	/// of UDP
 	/// </summary>
-	public class NetworkClient
+	public class NetworkClient : IDisposable
 	{
 		/// <summary>
 		/// IPEndPoint of the host client
 		/// </summary>
 		public IPEndPoint HostEndPoint { get; set; }
-		
+
 		/// <summary>
 		/// Frame of delays
 		/// </summary>
@@ -22,19 +28,99 @@ namespace BizHawk.Emulation.Common
 		/// <summary>
 		/// Port of this client.
 		/// </summary>
-		public int Port { get; set; }
+		public int ConsolePort { get; set; }
+		
+		/// <summary>
+		/// NetworkController used to interface with consoles
+		/// </summary>
+		public NetworkController NetworkController { get; set; }
+		
+		/// <summary>
+		/// Client used by the user. this class needs to delay inputs from the client to ensure that the games
+		/// are synced
+		/// </summary>
+		public IController UserController { get; set; }
+
+		private Stack<string> _inputStack = new Stack<string>();
+		private TcpClient _client;
+
+		private StreamReader _streamReader;
+		private StreamWriter _streamWriter;
 
 		/// <summary>
 		///
 		/// </summary>
 		/// <param name="hostEndPoint">End point of the host</param>
 		/// <param name="frameDelay">amount of frames before an input is registered</param>
-		/// <param name="port">port on the console of this client</param>
-		public NetworkClient(IPEndPoint hostEndPoint, int frameDelay, int port)
+		/// <param name="consolePort">port on the console of this client</param>
+		public NetworkClient(IPEndPoint hostEndPoint, IController userController, int frameDelay, int consolePort)
 		{
-			(HostEndPoint, FrameDelay, Port) = (hostEndPoint, frameDelay, port);
+			(HostEndPoint, UserController, FrameDelay, ConsolePort) = (hostEndPoint, userController, frameDelay, consolePort);
+			NetworkController = new NetworkController(userController, ConsolePort, 1);
+		}
+
+		/// <summary>
+		/// connects to a server using the HostEndPoint property of the obbject
+		/// </summary>
+		public void Connect() => Connect(HostEndPoint);
+
+		/// <summary>
+		/// connects to a server using a parameter
+		/// </summary>
+		/// <param name="endPoint">endpoint of the server</param>
+		public void Connect(IPEndPoint endPoint)
+		{
+			_client = new TcpClient(endPoint);
+			_streamReader = new StreamReader(_client.GetStream());
+			_streamWriter = new StreamWriter(_client.GetStream());
 		}
 		
-		public bool Update
+		/// <summary>
+		/// Updates the NetworkClient class. WARNING: WILL HANG WHEN WAITING FOR AN INPUT. ONLY USE WHEN RUNNING CORES
+		/// </summary>
+		public void Update(int frameCount)
+		{
+			if (frameCount > FrameDelay)
+			{
+				_inputStack.Push(NetworkController.ControllerToString());
+			}
+			else
+			{
+				//blank controller, don't accept inputs from either the user or the host
+				_inputStack.Push(NetworkController.GetBlankController(NetworkController.Definition, ConsolePort));
+				return;
+			}
+			
+			//get imputs from the input stack from the user and apply them to the output controller boject
+			NetworkController.StringToController(_inputStack.Pop());
+			
+			//read input from the stream reader and wait for input from the other end
+			string inputLn = _streamReader.ReadLine();
+			while (inputLn != null) inputLn = _streamReader.ReadLine();
+
+			//read the input and adjust the network controller
+			StringBuilder input = new StringBuilder();
+			while (inputLn != "END")
+			{
+				input.Append(inputLn);
+				inputLn = _streamReader.ReadLine();
+			}
+			NetworkController.StringToController(input.ToString());
+			
+			//send the input to the other client
+			_streamWriter.WriteLine(NetworkController.ControllerToString());
+
+		}
+
+		/// <summary>
+		/// disposes resources used by the network client and closes all connections
+		/// </summary>
+		public void Dispose()
+		{
+			_client?.Close();
+			_client?.Dispose();
+			_streamReader?.Dispose();
+			_streamWriter?.Dispose();
+		}
 	}
 }
--- src/BizHawk.Emulation.Common/Networking/NetworkController.cs
+++ src/BizHawk.Emulation.Common/Networking/NetworkController.cs
@@ -16,9 +16,24 @@ namespace BizHawk.Emulation.Common
 		public ControllerDefinition Definition { get; set; }
 		
 		/// <summary>
+		/// Frame delay before the inputs in the client controller are recgonized
+		/// </summary>
+		public int FrameDelay { get; set; }
+		
+		/// <summary>
 		/// Controller used by the user.
 		/// </summary>
-		public IController ClientController { get; set; }
+		public IController UserController { get; set; }
+		
+		/// <summary>
+		/// port that the user controls 
+		/// </summary>
+		public int UserPort { get; set; }
+		
+		/// <summary>
+		/// The port on the console that is used by the other networking client or host
+		/// </summary>
+		public int ClientPort { get; set; }
 		
 		public WorkingDictionary<string, bool> Buttons { get; set; }
 		public WorkingDictionary<string, int> Axes { get; set; }
@@ -26,31 +41,35 @@ namespace BizHawk.Emulation.Common
 		/// <summary>
 		///
 		/// </summary>
-		/// <param name="clientController">Controller that is controlled by the client</param>
-		/// <param name="definition">Controller Definition of both the client controller and this controller</param>
-		public NetworkController(IController clientController, ControllerDefinition definition)
+		/// <param name="clientController">Controller that is controlled by the user</param>
+		/// <param name="userPort">Port on the console where the controller controlled by the user is</param>
+		/// <param name="clientPort">Port on the console that is controlled by the other network client</param>
+		public NetworkController(IController clientController, int userPort, int clientPort)
 		{
-			(ClientController, Definition) = (clientController, definition);
+			(UserController, Definition, UserPort, ClientPort) = (clientController, clientController.Definition, userPort, clientPort);
 		}
 
 		/// <summary>
-		/// Converts an iController to string data that can beq parsed by network clients. Format is
+		/// Generates string data from the UserController that can beq parsed by network clients. Format is
 		/// ([B]utton/[A]xis) (Controller Port) (Name of Button/Axis) (Button/Axis value)
 		/// </summary>
-		/// <param name="port">Index/player of the controller to convert to string</param>
 		/// <returns>string data</returns>
-		public string ControllerToString(int port)
+		public string ControllerToString()
 		{
 			StringBuilder output = new StringBuilder();
-			foreach (string button in Definition.BoolButtons)
+			
+			//e[1] is the port number
+			foreach (string button in Definition.BoolButtons.Where(e => e[1] - '0' == UserPort))
 			{
-				output.Append($"B {port} {button} {IsPressed(button)}\n");
+				output.Append($"B{UserPort} {button} {UserController.IsPressed(button)}\n");
 			}
 
-			foreach (string axis in Definition.Axes.Keys)
+			foreach (string axis in Definition.Axes.Keys.Where(e => e[1] - '0' == UserPort))
 			{
-				output.Append($"A {port} {axis} {AxisValue(axis)}\n");
+				output.Append($"A{UserPort} {UserController.AxisValue(axis)}\n");
 			}
+
+			output.Append("END\n");
 
 			return output.ToString();
 		}
@@ -65,25 +84,49 @@ namespace BizHawk.Emulation.Common
 			{
 				string[] splitLn = line.Split(' ');
 
-				bool portValid = int.TryParse(splitLn[1], out int port);
-				string dataType = splitLn[0];
-				string dataName = splitLn[2];
+				int port = splitLn[0][1] - '0';
+				char dataType = splitLn[0][0];
+				string dataName = splitLn[1];
 				
-				if (dataType == "B" && bool.TryParse(splitLn[3], out bool boolVal) && portValid)
+				if (dataType == 'B' && bool.TryParse(splitLn[2], out bool boolVal))
 				{
 					Buttons[port + " " + dataName] = boolVal;
 				}
-				else if (splitLn[1] == "A" && int.TryParse(splitLn[3], out int axisValue) && portValid)
+				else if (dataType == 'A' && int.TryParse(splitLn[2], out int axisValue))
 				{
 					Axes[port + " " + dataName] = axisValue;
 				}
 				else
 				{
 					throw new InvalidDataException(
-						"Received string value is invalid. Format should be ([B]utton/[A]xis) (Controller Port) (Name of Button/Axis) (Button/Axis value)");
+						"Received string value is invalid. Format should be ([B]utton/[A]xis)(Controller Port) (Name of Button/Axis) (Button/Axis value)");
 				}
 			}
 		}
+
+		/// <summary>
+		/// gets a blank controller output with a definiation and a port
+		/// </summary>
+		/// <param name="definition">controller definitions. determines what axis and buttons to use</param>
+		/// <param name="port">console port number</param>
+		/// <returns></returns>
+		public static string GetBlankController(ControllerDefinition definition, int port)
+		{
+			StringBuilder output = new StringBuilder();
+			foreach (string button in definition.BoolButtons)
+			{
+				output.Append($"B {port} {button} false\n");
+			}
+
+			foreach (string axis in definition.Axes.Keys)
+			{
+				output.Append($"A {port} {axis} 0\n");
+			}
+
+			return output.ToString();
+		}
+
+
 
 		public bool IsPressed(string button) => Buttons[button];
 
